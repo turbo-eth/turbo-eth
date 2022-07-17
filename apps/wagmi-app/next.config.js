35a2d8885561a1dc3a0ec3726837173e70d79cb0
kames <kamescg@gmail.com>
2022-07-17 06:59:30 -0600

    fix

--- apps/wagmi-app/next.config.js
+++ apps/wagmi-app/next.config.js
@@ -7,6 +7,8 @@ module.exports = withBundleAnalyzer({
   eslint: {
     dirs: ['.'],
   },
+  assetPrefix: '/turbo-eth/',
+  basePath: '/turbo-eth',
   ignoreBuildErrors: true,
   poweredByHeader: false,
   trailingSlash: true,
